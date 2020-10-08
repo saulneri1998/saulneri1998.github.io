af681d7c56a06d131c94277f6b7914f76a0e9e96
Saul Neri <saul.nerio.1998@gmail.com>
2020-10-07 21:16:57 -0500

    style transfer ml5

---
+++ projects/style_transfer/precache-manifest.c382931cca8adcfe0906d58ebf5e4684.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e9aa96935b9ba1444891942950c7fa",
+    "url": "/projects/style_transfer/index.html"
+  },
+  {
+    "revision": "1164a6969d6d74199e7c",
+    "url": "/projects/style_transfer/static/js/2.6eed8537.chunk.js"
+  },
+  {
+    "revision": "26624b8331c21760682aea3e80184a25",
+    "url": "/projects/style_transfer/static/js/2.6eed8537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd35f7155bb255f1e1a9",
+    "url": "/projects/style_transfer/static/js/main.758d91fa.chunk.js"
+  },
+  {
+    "revision": "119fe9dc2b45e0bc1576",
+    "url": "/projects/style_transfer/static/js/runtime-main.9c16100b.js"
+  }
+]);
